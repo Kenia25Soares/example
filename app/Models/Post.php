1d863436e5ee1ee4d4adfc573a4051513ccae075
Kenia25Soares <keniadefreitasmorais@gmail.com>
2025-10-09 21:44:49 +0100

    Class 10 Model Factories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+  use HasFactory;
+  protected $table = 'post_listings';
          protected $fillable = ['title', 'body'];
 }
